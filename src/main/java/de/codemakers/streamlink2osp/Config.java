7b3cdd5a0bda2dc3b5834100547b4ec626959d0d
Paul Hagedorn <panzer1119@gmail.com>
2023-03-11 16:46:54 +0100

    feat: add variables TWITCH_CLIENT_ID and TWITCH_CLIENT_SECRET to Config.java

--- src/main/java/de/codemakers/streamlink2osp/Config.java
+++ src/main/java/de/codemakers/streamlink2osp/Config.java
@@ -18,6 +18,37 @@ package de.codemakers.streamlink2osp;
 
 public class Config {
 
+    // Keys
+    public static final String KEY_TWITCH_CLIENT_ID = "TWITCH_CLIENT_ID";
+    public static final String KEY_TWITCH_CLIENT_SECRET = "TWITCH_CLIENT_SECRET";
+
+    // Default Values
+    private static final String DEFAULT_TWITCH_CLIENT_ID = null;
+    private static final String DEFAULT_TWITCH_CLIENT_SECRET = null;
+
+    // Environment Values
+    private static final String ENV_TWITCH_CLIENT_ID = System.getenv(KEY_TWITCH_CLIENT_ID);
+    private static final String ENV_TWITCH_CLIENT_SECRET = System.getenv(KEY_TWITCH_CLIENT_SECRET);
+
+    // Values
+    private static final String VALUE_TWITCH_CLIENT_ID = ENV_TWITCH_CLIENT_ID != null ? ENV_TWITCH_CLIENT_ID : DEFAULT_TWITCH_CLIENT_ID;
+    private static final String VALUE_TWITCH_CLIENT_SECRET = ENV_TWITCH_CLIENT_SECRET != null ? ENV_TWITCH_CLIENT_SECRET : DEFAULT_TWITCH_CLIENT_SECRET;
+
+    private static String checkValue(String configValue, String configKey) {
+        if (configValue == null || configValue.isEmpty()) {
+            throw new RuntimeException(configKey + " is null or empty!");
+        }
+        return configValue;
+    }
+
+    public static String getTwitchClientId() {
+        return checkValue(VALUE_TWITCH_CLIENT_ID, KEY_TWITCH_CLIENT_ID);
+    }
+
+    public static String getTwitchClientSecret() {
+        return checkValue(VALUE_TWITCH_CLIENT_SECRET, KEY_TWITCH_CLIENT_SECRET);
+    }
+
     private Config() {
     }
 
