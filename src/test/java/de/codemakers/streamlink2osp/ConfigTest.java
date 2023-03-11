9298669b9b233f0ce7574cb5e14ec3153a730016
Paul Hagedorn <panzer1119@gmail.com>
2023-03-11 18:58:18 +0100

    test: update ConfigTest.java to use a list instead of an array for Twitch user ids and logins

--- src/test/java/de/codemakers/streamlink2osp/ConfigTest.java
+++ src/test/java/de/codemakers/streamlink2osp/ConfigTest.java
@@ -20,10 +20,9 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.Collectors;
+import java.util.List;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConfigTest {
@@ -31,16 +30,16 @@ class ConfigTest {
     // Generate random values for the environment variables with RandomStringUtils
     private static final String TWITCH_CLIENT_ID = RandomStringUtils.randomAlphanumeric(32);
     private static final String TWITCH_CLIENT_SECRET = RandomStringUtils.randomAlphanumeric(32);
-    private static final String TWITCH_USER_IDS = IntStream.range(0, 10).mapToObj(i -> RandomStringUtils.randomNumeric(8)).collect(Collectors.joining(","));
-    private static final String TWITCH_USER_LOGINS = IntStream.range(0, 10).mapToObj(i -> RandomStringUtils.randomAlphanumeric(12)).collect(Collectors.joining(","));
+    private static final List<String> TWITCH_USER_IDS = IntStream.range(0, 10).mapToObj(i -> RandomStringUtils.randomNumeric(8)).toList();
+    private static final List<String> TWITCH_USER_LOGINS = IntStream.range(0, 10).mapToObj(i -> RandomStringUtils.randomAlphanumeric(12)).toList();
 
     @BeforeAll
     static void setUp() {
         // Set the environment variables
         System.setProperty("TWITCH_CLIENT_ID", TWITCH_CLIENT_ID);
         System.setProperty("TWITCH_CLIENT_SECRET", TWITCH_CLIENT_SECRET);
-        System.setProperty("TWITCH_USER_IDS", TWITCH_USER_IDS);
-        System.setProperty("TWITCH_USER_LOGINS", TWITCH_USER_LOGINS);
+        System.setProperty("TWITCH_USER_IDS", String.join(",", TWITCH_USER_IDS));
+        System.setProperty("TWITCH_USER_LOGINS", String.join(", ", TWITCH_USER_LOGINS));
     }
 
     @Test
@@ -58,13 +57,13 @@ class ConfigTest {
     @Test
     void getTwitchUserIds() {
         // Check if the environment variable is set correctly
-        assertArrayEquals(TWITCH_USER_IDS.trim().split("\\s*,\\s*"), Config.getTwitchUserIds());
+        assertEquals(TWITCH_USER_IDS, Config.getTwitchUserIds());
     }
 
     @Test
     void getTwitchUserLogins() {
         // Check if the environment variable is set correctly
-        assertArrayEquals(TWITCH_USER_LOGINS.trim().split("\\s*,\\s*"), Config.getTwitchUserLogins());
+        assertEquals(TWITCH_USER_LOGINS, Config.getTwitchUserLogins());
     }
 
 }
